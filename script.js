bd8ca0c49f8105fe8d017218a951a8ce82d27c95
WalanS <teeranon@Walans-Macbook-Pro.local>
2026-06-19 22:28:05 +0700

    NALAW Version 3.0

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("NALAW Website Loaded");
+function toggleContact(){
+
+const menu =
+document.getElementById("floatingMenu");
+
+if(menu.style.display==="flex"){
+menu.style.display="none";
+}else{
+menu.style.display="flex";
+}
+
+}
